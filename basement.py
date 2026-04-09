00ab646cb7655252cf6295c680fd29787569eecd
usename00 <abdallahhashesh81@gmail.com>
2026-04-09 20:49:03 +0200

    Add files via upload

---
+++ basement.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def كسمك(ctx):
+    await ctx.send("كسمين امك")
+
+bot.run(os.getenv("ODA2OTYyMDI5NDQwMjcwMzQ3.GjTILb.BpDH1FbsU6UHCVxGbCMBllmEYKANsVAMbgv7NI"))
